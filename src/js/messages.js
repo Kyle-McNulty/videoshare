9cba964ed79d71a779e80a80df67002d8fb3ac67
Calvin <calvin.korver@gmail.com>
2016-12-02 15:27:28 -0800

    More formatting of the feed, commenting looks clean now

--- src/js/messages.js
+++ src/js/messages.js
@@ -30,163 +30,182 @@ var randomRef = firebase.database().ref("random");
 
 
 
-  var currentRef = storage.ref();
+var currentRef = storage.ref();
 
-  currentRef.child("videos/stevens.jpg").getDownloadURL().then(function (url) {
-    // Get the download URL for 'images/stars.jpg'
-    // This can be inserted into an <img> tag
-    // This can also be downloaded directly
-    console.log(url);
+currentRef.child("videos/stevens.jpg").getDownloadURL().then(function (url) {
+  // Get the download URL for 'images/stars.jpg'
+  // This can be inserted into an <img> tag
+  // This can also be downloaded directly
+  console.log(url);
 
-  }).catch(function (error) {
-    // Handle any errors
-  });
+}).catch(function (error) {
+  // Handle any errors
+});
 
-  var videoList = document.querySelector(".video-list");
+var videoList = document.querySelector(".video-list");
 
-  function handleFiles(fileList) {
-    if (currentUser.emailVerified) {
-      /* Iterates over the returned FileList object */
+function handleFiles(fileList) {
+  if (currentUser.emailVerified) {
+    /* Iterates over the returned FileList object */
     console.log("the list is ", fileList);
     var file = fileList[0];
-    console.log("file is ",file);
+    console.log("file is ", file);
     var storageRef = storage.ref(currentUser.uid + "/" + file.name);
     var uploadTask = storageRef.put(file); // adding to the storage 
-      uploadTask.then(function() { // adding to the database
-        // console.log(uploadTask.snapshot);
-        var info = {
-          createdOn: firebase.database.ServerValue.TIMESTAMP, //when created, filled in by Firebase
-          fileName: file.name,
-          downloadURL: uploadTask.snapshot.downloadURL,
-          createdBy: {
-            uid: currentUser.uid, //the unique user id
-            displayName: currentUser.displayName, //the user's display name
-            email: currentUser.email, //the user's email address
-            emailHashing: currentUser.photoURL, // 
-          }
-        };
-        personalRef.push(info);
-      })    // upload the file into storage
-    } else {
-      alert("You must verify your email before uploading"); //need to implement some more of this functionality
+    uploadTask.then(function () { // adding to the database
+      // console.log(uploadTask.snapshot);
+      var info = {
+        createdOn: firebase.database.ServerValue.TIMESTAMP, //when created, filled in by Firebase
+        fileName: file.name,
+        downloadURL: uploadTask.snapshot.downloadURL,
+        createdBy: {
+          uid: currentUser.uid, //the unique user id
+          displayName: currentUser.displayName, //the user's display name
+          email: currentUser.email, //the user's email address
+          emailHashing: currentUser.photoURL, // 
+        }
+      };
+      personalRef.push(info);
+    })    // upload the file into storage
+  } else {
+    alert("You must verify your email before uploading"); //need to implement some more of this functionality
+  }
+
+
+  uploadTask.on("state_changed", function (snapshot) {
+    // Observe state change events such as progress, pause, and resume
+    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+    var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    console.log('Upload is ' + progress + '% done');
+    switch (snapshot.state) {
+      case firebase.storage.TaskState.PAUSED: // or 'paused'
+        console.log('Upload is paused');
+        break;
+      case firebase.storage.TaskState.RUNNING: // or 'running'
+        console.log('Upload is running');
+        break;
     }
-    
+  }, function (error) {
+    // Handle unsuccessful uploads
+  }, function () {
+    // Handle successful uploads on complete
+    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+    var downloadURL = uploadTask.snapshot.downloadURL;
+    console.log(downloadURL);
+  });
+}
 
-    uploadTask.on("state_changed", function (snapshot) {
-      // Observe state change events such as progress, pause, and resume
-      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      console.log('Upload is ' + progress + '% done');
-      switch (snapshot.state) {
-        case firebase.storage.TaskState.PAUSED: // or 'paused'
-          console.log('Upload is paused');
-          break;
-        case firebase.storage.TaskState.RUNNING: // or 'running'
-          console.log('Upload is running');
-          break;
-      }
-    }, function (error) {
-      // Handle unsuccessful uploads
-    }, function () {
-      // Handle successful uploads on complete
-      // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-      var downloadURL = uploadTask.snapshot.downloadURL;
-      console.log(downloadURL);
-    });
+var generalRef = firebase.database().ref("general");
+
+
+/* 
+ * 
+ * Handles changing text on the material design lite upload selector
+ * 
+ * Courtesy of Alexander Gaziev from
+ * https://codepen.io/alexander-gaziev/pen/JdVQQm 
+ * 
+ **/
+var fileInputTextDiv = document.getElementById('file_input_text_div');
+var fileInput = document.getElementById('file_input_file');
+var fileInputText = document.getElementById('file_input_text');
+
+fileInput.addEventListener('change', changeInputText);
+fileInput.addEventListener('change', changeState);
+
+function changeInputText() {
+  var str = fileInput.value;
+  var i;
+  if (str.lastIndexOf('\\')) {
+    i = str.lastIndexOf('\\') + 1;
+  } else if (str.lastIndexOf('/')) {
+    i = str.lastIndexOf('/') + 1;
   }
+  fileInputText.value = str.slice(i, str.length);
+}
 
-  var generalRef = firebase.database().ref("general");
-
-
-  /* 
-   * 
-   * Handles changing text on the material design lite upload selector
-   * 
-   * Courtesy of Alexander Gaziev from
-   * https://codepen.io/alexander-gaziev/pen/JdVQQm 
-   * 
-   **/
-  var fileInputTextDiv = document.getElementById('file_input_text_div');
-  var fileInput = document.getElementById('file_input_file');
-  var fileInputText = document.getElementById('file_input_text');
-
-  fileInput.addEventListener('change', changeInputText);
-  fileInput.addEventListener('change', changeState);
-
-  function changeInputText() {
-    var str = fileInput.value;
-    var i;
-    if (str.lastIndexOf('\\')) {
-      i = str.lastIndexOf('\\') + 1;
-    } else if (str.lastIndexOf('/')) {
-      i = str.lastIndexOf('/') + 1;
+function changeState() {
+  if (fileInputText.value.length != 0) {
+    if (!fileInputTextDiv.classList.contains("is-focused")) {
+      fileInputTextDiv.classList.add('is-focused');
     }
-    fileInputText.value = str.slice(i, str.length);
-  }
-
-  function changeState() {
-    if (fileInputText.value.length != 0) {
-      if (!fileInputTextDiv.classList.contains("is-focused")) {
-        fileInputTextDiv.classList.add('is-focused');
-      }
-    } else {
-      if (fileInputTextDiv.classList.contains("is-focused")) {
-        fileInputTextDiv.classList.remove('is-focused');
-      }
+  } else {
+    if (fileInputTextDiv.classList.contains("is-focused")) {
+      fileInputTextDiv.classList.remove('is-focused');
     }
   }
+}
+
+/* This function renders out each move that is in Firebase storage */
+function renderMovie(snapshot) {
+
+  /* Grabs the element from Firebase Storage */
+  var element = snapshot.val();
+  var cell = document.createElement("div");
+  cell.setAttribute("class", "demo-card-wide mdl-card mdl-shadow--2dp video-cell");
+
+  /* Handles creation of the video element */
+  var media = document.createElement("div");
+  var source = document.createElement('source');
+  var video = document.createElement("video");
+  media.setAttribute("class", "mdl-card__media");
+  video.setAttribute("controls", "true");
+  video.setAttribute("width", "100%");  // should change
+  video.setAttribute("height", "90%");
+  // console.log("element is: ");
+  // console.log(element);
+  source.setAttribute('src', element.downloadURL);
+  video.appendChild(source);
+
+  /* Title of the Video */
+  var titleDiv = document.createElement("div");
+  titleDiv.setAttribute("class", "mdl-card__title");
+  var title = document.createElement("h2");
+  title.setAttribute("class", "mdl-card__title-text");
+  title.innerHTML = element.fileName
+    .substring(0, element.fileName.length - 4);  // cuts off .mp4
 
 
-  function renderMovie(snapshot) {
+  /* Author */
+  var authorDiv = document.createElement("div");
+  authorDiv.setAttribute("class", "mdl-card__supporting-text");
+  var author = document.createElement("p");
+  var name = element.createdBy.displayName;
+   var description = "This is a description of the video that can be added in by the user using the metadata property";
+   var br = document.createElement("br");
+    author.innerHTML = "Uploaded by " + name.bold();
+    author.appendChild(br);
+    author.innerHTML += description;
+  
+  /* Video Description */
+  // var descriptionDiv = document.createElement("div");
+  // descriptionDiv.setAttribute("class", "mdl-card__supporting-text");
 
-    var element = snapshot.val();
-    var cell = document.createElement("div");
-    cell.setAttribute("class", "demo-card-wide mdl-card mdl-shadow--2dp video-cell");
-    
+  authorDiv.appendChild(author);
+  // authorDiv.appendChild(description);
 
-    var media = document.createElement("div");
-    media.setAttribute("class", "mdl-card__media");
-    var video = document.createElement("video");
-    video.setAttribute("controls", "true");
-    video.setAttribute("width", "100%");  // should change
-    video.setAttribute("height", "90%");
+  /* Appends all child elements to the main video cell object */
+  titleDiv.appendChild(title);
+  media.appendChild(video);
+  cell.appendChild(media);
+  cell.appendChild(titleDiv);
+  cell.appendChild(authorDiv);
+  // cell.appendChild(descriptionDiv);
 
-    var source = document.createElement('source');
-    console.log("element is: ");
-    console.log(element);
-    source.setAttribute('src', element.downloadURL);
-    video.appendChild(source);
-    //video.play();
-    var titleDiv = document.createElement("div");
-    titleDiv.setAttribute("class", "mdl-card__title");
-    var title = document.createElement("h2");
-    title.setAttribute("class", "mdl-card__title-text");
-    title.innerHTML = element.fileName.substring(0, element.fileName.length - 4);  // title for each video
+  /* Appends the cell to the entire feed of videos */
+  videoList.appendChild(cell);
+}
 
-    var descriptionDiv = document.createElement("div");
-    descriptionDiv.setAttribute("class", "mdl-card__supporting-text")
-    var description = document.createElement("p");
-    description.innerHTML = "This is a description of the video that can be added in by the user using the metadata property";
-    descriptionDiv.appendChild(description);
+/* Renders each snapshot in the storage by calling the renderMovie method for each snapshot that we get */
+function render(snapshot) {
+  videoList.innerHTML = "";
+  snapshot.forEach(renderMovie);
+}
 
-    titleDiv.appendChild(title);
-    media.appendChild(video);
-    cell.appendChild(media);
-    cell.appendChild(titleDiv);
-    cell.appendChild(descriptionDiv);
-
-    videoList.appendChild(cell);
-  }
-
-  function render(snapshot) {
-    videoList.innerHTML = "";
-    snapshot.forEach(renderMovie);
-  }
-
-  /* Changes the page to profile page from button input in nav bar */
-document.getElementById("profile-page-button").addEventListener("click", function (){
+/* Changes the page to profile page from button input in nav bar */
+document.getElementById("profile-page-button").addEventListener("click", function () {
   console.log("Changing page");
   window.location = "../profile.html";
 });
 
-personalRef.limitToLast(100).on("value", render);
+personalRef.limitToLast(20).on("value", render);
